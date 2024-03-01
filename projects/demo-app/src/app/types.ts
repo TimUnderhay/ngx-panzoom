0b3c67d02beecc426247dc84851ab70426b58f85
Tim Underhay <15734900+KensingtonTech@users.noreply.github.com>
2024-02-29 20:32:25 -0500

    Added demo app project.

---
+++ projects/demo-app/src/app/types.ts
@@ -0,0 +1,5 @@
+export interface Content {
+  contentFile: string;
+  thumbnail: string;
+  id: number;
+}
--- projects/ngx-panzoom/src/lib/panzoom.component.ts
+++ projects/ngx-panzoom/src/lib/panzoom.component.ts
@@ -4,7 +4,6 @@ import {
   AfterViewInit,
   OnDestroy,
   ElementRef,
-  ViewChild,
   viewChild,
   NgZone,
   model,
